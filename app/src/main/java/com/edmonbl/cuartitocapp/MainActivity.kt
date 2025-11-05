09952cfb2d473eafab2be568c18c248d84065534
JORED666 <243842@ids.upchiapas.edu.mx>
2025-11-05 14:58:02 -0600

    add MainActivity.kt functionality

--- app/src/main/java/com/edmonbl/cuartitocapp/MainActivity.kt
+++ app/src/main/java/com/edmonbl/cuartitocapp/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.edmonbl.cuartitocapp
 
-class MainActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.edmonbl.cuartitocapp.ui.theme.CuartitoCAppTheme
 
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CuartitoCAppTheme {
+                AppNavigation()
+            }
+        }
+    }
 }
