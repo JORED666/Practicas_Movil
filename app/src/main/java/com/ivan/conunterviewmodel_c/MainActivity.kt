d535a8fa413cd227993307edfccc04462c041d68
JORED666 <243842@ids.upchiapas.edu.mx>
2025-11-05 14:23:55 -0600

    Actividad 3 - ViewModel

---
+++ app/src/main/java/com/ivan/conunterviewmodel_c/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.ivan.conunterviewmodel_c
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ivan.conunterviewmodel_c.ui.theme.ConunterViewModel_CTheme
+import viewModels.CounterViewModels
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val counterViewModels: CounterViewModels by viewModels()
+        enableEdgeToEdge()
+        setContent {
+            ConunterViewModel_CTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Content(paddingValues = innerPadding, counterViewModels = counterViewModels)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Content(paddingValues: PaddingValues, counterViewModels: CounterViewModels) {
+    //var counter = 0
+    Column (
+        modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(text = "Counter: ${counterViewModels.counter.value}")
+        Spacer(modifier = Modifier.height(height = 10.dp))
+        Button(
+            onClick = {
+                counterViewModels.add()
+            }
+        ) {
+            Text(text = "+")
+        }
+    }
+}
---
+++ app/src/main/java/viewModels/CounterViewModels.kt
@@ -0,0 +1,13 @@
+package viewModels
+
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.lifecycle.ViewModel
+
+class CounterViewModels: ViewModel() {
+    private val _countent = mutableIntStateOf(value = 0)
+    val counter = _countent
+
+    fun add() {
+        _countent.value++
+    }
+}
